f1351f498583467f5f4d922c67ec1d7fd9c2d2c9
Ajay Singh <100398190+ajaysingh923@users.noreply.github.com>
2024-08-08 11:51:45 +0545

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -18,13 +18,13 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'nischal.201720@ncit.edu.np';                     //SMTP username
-    $mail->Password   = 'ojdckkrwsumzunrl ';                               //SMTP password
+    $mail->Username   = 'youremail@gmail.com';                     //SMTP username
+    $mail->Password   = 'your password ';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('nischal.201720@ncit.edu.np', 'Nischal Dhakal');
+    $mail->setFrom('youremail@gmail.com', 'Nischal Dhakal');
     $mail->addAddress($_POST['email']);               //Name is optional
 
     
